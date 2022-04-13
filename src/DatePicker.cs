d3a666e0778034ab65d114e00a51d5ed63c1bded
Kai Delay <kbde@riseup.net>
2022-04-13 18:10:53 +0200

    base files added

---
+++ src/DatePicker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSceduler
+{
+    public class DatePicker
+    {
+        private DateTime _selectedTime = DateTime.MinValue;
+        public DateTime SelectedTime
+        {
+            get { return _selectedTime; }
+            set
+            {
+                _selectedTime = value.AddDays(1 - (double)value.DayOfWeek);
+            }
+        }
+
+        public string Month
+        {
+            get { return SelectedTime.ToString("MMMM yyyy"); }
+        }
+    }
+}
---
+++ src/Scheduler.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleSceduler
+{
+    /// <summary>
+    /// Interaction logic for Scheduler.xaml
+    /// </summary>
+    public partial class Scheduler : UserControl
+    {
+        ViewModel vm;
+
+        public Scheduler()
+        {
+            vm = new();
+            InitializeComponent();
+            this.DataContext = vm;
+            vm.DatePicker.SelectedTime = DateTime.Now;
+        }
+    }
+}
